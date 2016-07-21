a3e3cee8607e2527e548dbba80004e8f6a186674
Oz Linden <oz@lindenlab.com>
2016-07-21 11:00:00 -0400

    clean up packaging, clarify build commands

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Run the script provided by curl that downloads and converts the Mozilla certificate authorities
 perl ./packages/scripts/mk-ca-bundle.pl
 
 date "+%Y.%m.%d.${revision:-0}" > VERSION.txt
