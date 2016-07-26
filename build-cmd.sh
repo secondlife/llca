f71c7ba4067c4723eca875a040870d40b882f681
Oz Linden <oz@lindenlab.com>
2016-07-26 13:36:14 -0400

    directly download the script from curl, removing the autobuild package dependency

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
+exec 3>&1; export BASH_XTRACEFD=3; set -x
+ 
+# This fetches an xml file that contains a pointer to the latest curl release tarball
+curl -s 'https://curl.haxx.se/metalink.cgi?curl=tar.gz' > metalink.xml
+
+# The use of a default xml namespace confuses the xmllint xpath processor, so remove it
+xmllint --format metalink.xml | sed 's/xmlns="urn:ietf:params:xml:ns:metalink"//' > metalink_nons.xml
+
+# Extract the name url of the latest release tarball
+curl_release_tarball_name=$(xmllint --xpath 'string(/metalink/file/@name)' metalink_nons.xml)
+curl_release_tarball_url=$(xmllint --xpath '/metalink/file/url[1]/text()' metalink_nons.xml)
+
+# Fetch the tarball
+curl -s "${curl_release_tarball_url}" > "${curl_release_tarball_name}"
+
+# Extract the script mk-ca-bundle.pl from the tarball
+curl_release_dir=$(echo "${curl_release_tarball_name}" | sed 's/.tar.gz//')
+tar -x -z --strip-components 2 -f "${curl_release_tarball_name}" "${curl_release_dir}/lib/mk-ca-bundle.pl"
+
 # Run the script provided by curl that downloads and converts the Mozilla certificate authorities
-perl ./packages/scripts/mk-ca-bundle.pl
+perl ./mk-ca-bundle.pl
 
 # Use this date on which we did this download as the bundle version number
 date "+%Y.%m.%d.${revision:-0}" > VERSION.txt
 
 cp -r ../LICENSES .
-
-# HACK WARNING !
-#
-# The real output of this package is the ca-bundle.txt file, which is platform independent,
-# so we build the package on Linux because it already has all the perl support needed
-# by mk-ca-bundle.pl, but as platform 'common' and use the output of that one build on all
-# platforms. This avoids the possibility of different bundles on different platforms, and
-# the need to install the requisite perl support in our CYGWIN (Windows) build hosts.
-#
-# This would create a problem because this package depends directly on curl for the
-# mk-ca-buindle.pl script (and so indirectly on zlib and openssl), which are platform dependent.
-# If, as is normally the case, this package is built on the Linux platform and then resulting
-# llca package is installed in a CYGWIN build, autobuild will fail because it will see a conflict
-# between the CYGWIN builds of curl, zlib, and openssl and the Linux versions of those packages
-# it would find in the dependencies of this package.
-#
-# We overcome this by overwriting the installed-packages file here with one that contains no
-# dependencies so that the resulting llca package will not create conflicts on any platform
-# except with some other version of itself.
-cp ../no-depends.xml ./packages/installed-packages.xml
