84aa5e6a9906a5257d346e2c62ff22f6a8211ab6
Andrey Lihatskiy <alihatskiy@productengine.com>
2021-08-30 04:33:34 +0300

    SL-15775 Fetch the mk-ca-bundle.pl script directly from github

--- build-cmd.sh
+++ build-cmd.sh
@@ -4,22 +4,9 @@
 set -e
 set -x
 
-# Fetch a pointer to the latest curl release tarball from the curl distribution site
-curl -s 'https://curl.se/metalink.cgi?curl=tar.gz' > metalink.xml
+# Fetch mk-ca-bundle.pl from github
+curl -s 'https://raw.githubusercontent.com/curl/curl/master/lib/mk-ca-bundle.pl' -o './mk-ca-bundle.pl'
 
-# The use of a default xml namespace confuses the xmllint xpath processor, so remove it
-xmllint --format metalink.xml | sed 's/xmlns="urn:ietf:params:xml:ns:metalink"//' > metalink_no_namespace.xml
-
-# Extract the name and url of the latest release tarball
-curl_release_tarball_name=$(xmllint --xpath 'string(/metalink/file/@name)' metalink_no_namespace.xml)
-curl_release_tarball_url=$(xmllint --xpath '/metalink/file/url[1]/text()' metalink_no_namespace.xml)
-
-# Fetch the curl distribution tarball
-curl -s "${curl_release_tarball_url}" > "${curl_release_tarball_name}"
-
-# Extract the script mk-ca-bundle.pl from the curl distribution
-curl_release_dir=$(echo "${curl_release_tarball_name}" | sed 's/.tar.gz//')
-tar -x -z --strip-components 2 -f "${curl_release_tarball_name}" "${curl_release_dir}/lib/mk-ca-bundle.pl"
 # Patch mk-ca-bundle.pl to add time validity tests
 patch < ../mk-ca-bundle.patch
 
